72d7572b5773e5be95e0e7aeafa47829da020867
Bence A. Tóth <tothab@gmail.com>
2021-05-23 18:18:46 +0200

    Added to-do items

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import Settings from "./Settings";
 
 import "./App.css";
 
+// TODO: Add unit tests:
+// TODO: Test that CheckIn component calls onAddCheckIn with correct value
+// TODO: Test that CheckIn component calls onToggleCheckIns
+// TODO: Test that CheckIns component shows no check-ins message
+// TODO: Test that CheckIns component renders average check-ins and lists check-ins
+// TODO: Test that average utility works as expected
+// TODO: Test that Settings component toggles dark mode when clicked
+// TODO: Check that hamburger menu toggles settings when clicked
+
 const App = () => {
   const { checkIns, onAddCheckIn } = useCheckIns();
   const { areCheckInsVisible, onToggleCheckIns } = useCheckInsToggle();
