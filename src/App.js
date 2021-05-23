dd31ea51c9dc0ae124f5bc51f34d9d5c2f757b4c
Bence A. Tóth <tothab@gmail.com>
2021-05-23 18:45:39 +0200

    Added tests for CheckIn component

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Settings from "./Settings";
 import "./App.css";
 
 // TODO: Add unit tests:
-// TODO: Test that CheckIn component calls onAddCheckIn with correct value
-// TODO: Test that CheckIn component calls onToggleCheckIns
 // TODO: Test that CheckIns component shows no check-ins message
 // TODO: Test that CheckIns component renders average check-ins and lists check-ins
 // TODO: Test that average utility works as expected
---
+++ src/CheckIn.test.js
@@ -0,0 +1,64 @@
+import { render, screen } from "@testing-library/react";
+import CheckIn from "./CheckIn";
+
+test("calls onAddCheckIn with correct value for awful", () => {
+  const onAddCheckIn = jest.fn();
+  render(<CheckIn onAddCheckIn={onAddCheckIn} />);
+
+  const awfulButton = screen.getByRole("button", { name: /awful/i });
+  awfulButton.click();
+
+  expect(onAddCheckIn).toBeCalledWith(1);
+});
+
+test("calls onAddCheckIn with correct value for bad", () => {
+  const onAddCheckIn = jest.fn();
+  render(<CheckIn onAddCheckIn={onAddCheckIn} />);
+
+  const badButton = screen.getByRole("button", { name: /bad/i });
+  badButton.click();
+
+  expect(onAddCheckIn).toBeCalledWith(2);
+});
+
+test("calls onAddCheckIn with correct value for okay", () => {
+  const onAddCheckIn = jest.fn();
+  render(<CheckIn onAddCheckIn={onAddCheckIn} />);
+
+  const okayButton = screen.getByRole("button", { name: /okay/i });
+  okayButton.click();
+
+  expect(onAddCheckIn).toBeCalledWith(3);
+});
+
+test("calls onAddCheckIn with correct value for good", () => {
+  const onAddCheckIn = jest.fn();
+  render(<CheckIn onAddCheckIn={onAddCheckIn} />);
+
+  const goodButton = screen.getByRole("button", { name: /good/i });
+  goodButton.click();
+
+  expect(onAddCheckIn).toBeCalledWith(4);
+});
+
+test("calls onAddCheckIn with correct value for awesome", () => {
+  const onAddCheckIn = jest.fn();
+  render(<CheckIn onAddCheckIn={onAddCheckIn} />);
+
+  const awesomeButton = screen.getByRole("button", { name: /awesome/i });
+  awesomeButton.click();
+
+  expect(onAddCheckIn).toBeCalledWith(5);
+});
+
+test("calls onToggleCheckIns when toggle button is clicked", () => {
+  const onToggleCheckIns = jest.fn();
+  render(<CheckIn onToggleCheckIns={onToggleCheckIns} />);
+
+  const toggleCheckInsButton = screen.getByRole("button", {
+    name: /show previous check-ins/i,
+  });
+  toggleCheckInsButton.click();
+
+  expect(onToggleCheckIns).toBeCalled();
+});
