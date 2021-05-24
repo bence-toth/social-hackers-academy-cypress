e3281cdcd5b66eb6947f5cb6847d18acf10e75b3
Bence A. Tóth <tothab@gmail.com>
2021-05-24 14:56:46 +0200

    Created Cypress test outlines

---
+++ cypress/integration/checkIns.spec.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+context("Mood tracking", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("Check-ins interface expands and collapses", () => {});
+
+  it("Check-ins are registered when check-ins interface is closed", () => {});
+
+  it("Check-ins are registered when check-ins interface is open", () => {});
+});
---
+++ cypress/integration/settings.spec.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+context("Settings", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("Dark mode can be turned on and off", () => {});
+});
